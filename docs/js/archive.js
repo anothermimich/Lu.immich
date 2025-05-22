8c2701d40947a597516f5e4b14e8d4e1aba87294
anothermimich <lucas.immich@outlook.com>
2025-05-22 14:43:17 -0300

    something else

--- docs/js/archive.js
+++ docs/js/archive.js
@@ -2,9 +2,29 @@
   var grid;
   function init() {
     grid = new Minigrid({
-      container: ".archive-item-container",
+      container: "#archive-item-container-2025",
       item: ".archive-item",
-      gutter: 22,
+      gutter: 32,
+    });
+    grid.mount();
+  }
+
+  // mount
+  function update() {
+    grid.mount();
+  }
+
+  document.addEventListener("DOMContentLoaded", init);
+  window.addEventListener("resize", update);
+})();
+
+(function () {
+  var grid;
+  function init() {
+    grid = new Minigrid({
+      container: "#archive-item-container-2024",
+      item: ".archive-item",
+      gutter: 32,
     });
     grid.mount();
   }
