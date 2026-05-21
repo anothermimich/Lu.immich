9840c782431cfc0b06d15cb3c3e41b5940638245
anothermimich <lucas.immich@outlook.com>
2026-05-21 17:48:11 -0300

    margin home

--- docs/js/home.js
+++ docs/js/home.js
@@ -29,7 +29,7 @@ function ajustarHeroDinamicamente() {
     mobileBreakpoint: 1000,
     maxMobileItens: 5,
     margemMinimaDesktop: 80,
-    margemMinimaMobile: 120,
+    margemMinimaMobile: 110,
   };
   // ==========================================
 
